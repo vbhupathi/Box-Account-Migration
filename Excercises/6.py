f2399b4ed168a04f9cbf8707692c8126850ca4ba
vbhupathi <50917935+vbhupathi@users.noreply.github.com>
2021-01-29 22:02:21 -0500

    Excercise 6

---
+++ Excercises/6.py
@@ -0,0 +1,13 @@
+# Excercise 6
+# Ask the user for a string and print out whether this string is a palindrome or not. 
+# (A palindrome is a string that reads the same forwards and backwards.)
+
+string = input(" Enter a Palindrome string: ")
+string=str(string)
+backwordString=string[::-1]
+print(backwordString)
+
+if string == backwordString :
+    print(string + " is a palindrome")
+else :
+    print(string + " is not a palindrome")
