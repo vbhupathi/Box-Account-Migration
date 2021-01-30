c4a2c15e6fd693a76cc3ab9aab1db4540dc2f153
vbhupathi <50917935+vbhupathi@users.noreply.github.com>
2021-01-29 22:12:12 -0500

    Exercise 2

--- Exercises/2.py
+++ Exercises/2.py
@@ -1,3 +1,4 @@
+# Exercise 2
 # Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user.
 number = int(input("Enter a number: "))
 if number%2 == 0: print("the entered " + str(number) + " is even number")
