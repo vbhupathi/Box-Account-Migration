37512b0e2042b5a395b4b66082378977f115ea8c
vbhupathi <50917935+vbhupathi@users.noreply.github.com>
2021-01-29 22:13:14 -0500

    Exercise 6

--- Exercises/6.py
+++ Exercises/6.py
@@ -1,4 +1,4 @@
-# Excercise 6
+# Exercise 6
 # Ask the user for a string and print out whether this string is a palindrome or not. 
 # (A palindrome is a string that reads the same forwards and backwards.)
 
